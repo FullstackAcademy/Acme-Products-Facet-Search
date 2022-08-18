ea01d8497359506032199578a7fd7693c50946eb
ericpkatz <ericpkatz@gmail.com>
2022-08-18 11:56:51 -0400

    implement facet search

--- src/index_start.js
+++ src/index_start.js
@@ -27,21 +27,81 @@ const Search = connect(
   }
 )(({ products, match })=> {
   const filter = match.params.filter ? JSON.parse(match.params.filter): {};
+  const filtered = products
+    .filter(product => !filter.colorId || filter.colorId === product.colorId) 
+    .filter(product => !filter.sizeId || filter.sizeId === product.sizeId) 
+
+
+  const colorMap = products
+    .filter(product => !filter.sizeId || filter.sizeId === product.sizeId)
+    .reduce((acc, product)=> {
+    const id = product.color.id;
+    acc[id] = acc[id] || { id, name: product.color.name, count: 0 };
+    acc[id].count++;
+    return acc;
+  }, {});
+  const colorEntries = Object.values(colorMap);
+  
+  const sizeMap = products
+    .filter(product => !filter.colorId || filter.colorId === product.colorId)
+    .reduce((acc, product)=> {
+    const id = product.size.id;
+    acc[id] = acc[id] || { id, name: product.size.name, count: 0 };
+    acc[id].count++;
+    return acc;
+  }, {});
+  const sizeEntries = Object.values(sizeMap);
 
   return (
     <div id='search'>
       <section>
         <div>
           <h1>Color Facet</h1>
+          <ul>
+            {
+              colorEntries.map( entry => {
+                const _filter = { ...filter, colorId: entry.id };
+                if(_filter.colorId === filter.colorId){
+                  delete _filter.colorId;
+                }
+                const url = `/search/${JSON.stringify(_filter)}`;
+                return (
+                  <li key = { entry.id } className={ entry.id === filter.colorId ? 'selected': ''}>
+                    <Link to={ url }>
+                      { entry.name } ({ entry.count })
+                    </Link>
+                  </li>
+                );
+              })
+            }
+          </ul>
         </div>
         <div>
           <h1>Size Facet</h1>
+          <ul>
+            {
+              sizeEntries.map( entry => {
+                const _filter = {...filter, sizeId: entry.id };
+                if(_filter.sizeId === filter.sizeId){
+                  delete _filter.sizeId;
+                }
+                const url = `/search/${JSON.stringify(_filter)}`;
+                return (
+                  <li key = { entry.id } className={ entry.id === filter.sizeId ? 'selected': ''}>
+                    <Link to={ url }>
+                      { entry.name } ({ entry.count })
+                    </Link>
+                  </li>
+                );
+              })
+            }
+          </ul>
         </div>
       </section>
       <section>
         <ul>
           {
-            products.map( product => {
+            filtered.map( product => {
               return (
                 <li
                   key={ product.id}
@@ -93,7 +153,7 @@ const App = connect(
         </nav>
         <main>
           <h1>Acme Products ({ count })</h1>
-          <Route path='/Search/:filter?' component={ Search } />
+          <Route path='/search/:filter?' component={ Search } />
         </main>
       </div>
     );
