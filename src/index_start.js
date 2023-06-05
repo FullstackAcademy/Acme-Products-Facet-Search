f2f926aa27d06e90d7c5649116bcb1b48be21d33
ericpkatz <ericpkatz@gmail.com>
2023-06-05 19:18:31 -0400

    facet-search working

--- src/index_start.js
+++ src/index_start.js
@@ -30,21 +30,89 @@ const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 const Search = ()=> {
   const { products } = useSelector(state => state);
+  const { filterString } = useParams();
+  const filter = filterString ? JSON.parse(filterString) : {};
+
+  const colorMap = products
+    .filter(product => !filter.size || filter.size === product.sizeId)
+    .reduce((acc, product)=> {
+    const key = product.colorId;
+    acc[key] = acc[key] || { id: product.colorId, name: product.color.name, count: 0 };
+    acc[key].count++;
+    return acc;
+  }, {});
+
+  const colors = Object.values(colorMap);
+
+  const sizeMap = products
+    .filter(product => !filter.color || filter.color === product.colorId)
+    .reduce((acc, product)=> {
+    const key = product.sizeId;
+    acc[key] = acc[key] || { id: product.sizeId, name: product.size.name, count: 0 };
+    acc[key].count++;
+    return acc;
+  }, {});
+
+  const sizes = Object.values(sizeMap);
+
+  const filtered = products
+    .filter(product => !filter.color || filter.color === product.colorId)
+    .filter(product => !filter.size || filter.size === product.sizeId)
 
   return (
     <div id='search'>
       <section>
         <div>
           <h1>Color Facet</h1>
+          <ul>
+            {
+              colors.map( color => {
+                const _filter = {...filter };
+                if(filter.color === color.id){
+                  delete _filter.color;
+                }
+                else {
+                  _filter.color = color.id;
+                }
+                return (
+                  <li key={ color.id } style={{ fontWeight: filter.color === color.id ? 'bold': ''}}>
+                    <Link to={ `/${JSON.stringify(_filter)}`}>
+                    { color.name } ({ color.count })
+                    </Link>
+                  </li>
+                );
+              })
+            }
+          </ul>
         </div>
         <div>
           <h1>Size Facet</h1>
+          <ul>
+            {
+              sizes.map( size => {
+                const _filter = {...filter };
+                if(filter.size === size.id){
+                  delete _filter.size;
+                }
+                else {
+                  _filter.size = size.id;
+                }
+                return (
+                  <li key={ size.id } style={{ fontWeight: filter.size === size.id ? 'bold': ''}}>
+                    <Link to={ `/${JSON.stringify(_filter)}`}>
+                    { size.name } ({ size.count })
+                    </Link>
+                  </li>
+                );
+              })
+            }
+          </ul>
         </div>
       </section>
       <section>
         <ul>
           {
-            products.map( product => {
+            filtered.map( product => {
               return (
                 <li
                   key={ product.id}
@@ -84,6 +152,7 @@ const App = ()=> {
           <h1>Acme Products ({ products.length })</h1>
           <Routes>
             <Route path='/' element={ <Search /> } />
+            <Route path='/:filterString' element={ <Search /> } />
           </Routes>
         </main>
       </div>
